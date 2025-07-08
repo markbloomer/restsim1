ef576f37a27c5ca724ed17a63147557eee054784
Mark Bloomer <mark.bloomer@3pillarglobal.com>
2025-07-08 10:49:22 -0400

    add seed data; create and cleanup scripts

--- api/src/db/seeders/seed.ts
+++ api/src/db/seeders/seed.ts
@@ -1,6 +1,40 @@
 // Placeholder for database seeding script
 
-export async function seed() {
-  // TODO: Add seeding logic
-  console.log('Seeding database...');
+import prisma from '../index';
+
+async function main() {
+  // Create Users
+  await prisma.user.createMany({
+    data: [
+      {
+        email: 'admin@example.com',
+        username: 'admin',
+        password: 'password',
+        role: 'ADMIN',
+      },
+      {
+        email: 'instructor@example.com',
+        username: 'instructor1',
+        password: 'password',
+        role: 'INSTRUCTOR',
+      },
+      {
+        email: 'team1@example.com',
+        username: 'team1',
+        password: 'password',
+        role: 'TEAM',
+      },
+    ],
+    skipDuplicates: true,
+  });
+  console.log('Seeded users!');
 }
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
